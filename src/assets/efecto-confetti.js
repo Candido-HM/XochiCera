5e127f676bfcfcb63ebe4baefd4aa873b1b96f09
Candido Hernandez <candidohm99cursos@gmail.com>
2025-01-03 17:35:25 -0600

    animacion de confetti

---
+++ src/assets/efecto-confetti.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var end = Date.now() + (6 * 1000);
+  
+    // Definir colores
+    var colors = ['#511252', '#262626'];
+  
+    (function frame() {
+      confetti({
+        particleCount: 2,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 },
+        colors: colors
+      });
+      confetti({
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1 },
+        colors: colors
+      });
+  
+      if (Date.now() < end) {
+        requestAnimationFrame(frame);
+      }
+    }());
+  });
